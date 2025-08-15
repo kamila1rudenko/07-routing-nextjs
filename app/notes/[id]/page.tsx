dfe4d66874f45a36d5b3d7a0b65a7067b64f8640
Kamila <kamillkaa95@gmail.com>
2025-08-15 16:48:09 +0300

    conflkts:

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -7,17 +7,18 @@ import { fetchNoteById } from '@/lib/api';
 import NoteDetailsClient from './NoteDetails.client';
 
 type NoteDetailsProps = {
-  params: Promise<{ id: string }>;
+  params: { id: string }; 
 };
 
 export default async function NotePage({ params }: NoteDetailsProps) {
-  const { id } = await params;
+  const { id } = params; 
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
     queryKey: ['note', id],
     queryFn: () => fetchNoteById(id),
   });
+
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
       <NoteDetailsClient />
